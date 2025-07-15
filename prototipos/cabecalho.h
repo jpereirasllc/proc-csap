96c0eaef862fc1db793e4852f4e5f5afd0605fb1
Jeoás Silva <contatojeoassilva@gmail.com>
2025-07-15 11:05:10 -0400

    Create cabecalho.h

---
+++ prototipos/cabecalho.h
@@ -0,0 +1,16 @@
+
+
+#ifndef CABECALHO_H_INCLUDED
+#define CABECALHO_H_INCLUDED
+
+void entradaDadosUsuario(int opcao);
+
+void seletorProcessosDadosUsuario(int opcao,char mensagem[],char chave[]);
+
+void modificaComprimentoDaMensagem(char mensagem[]);
+
+void modificaComprimentoDaChave(char mensagem[],char chave[]);
+
+void seletorNumerosDadosUsuario(char dadosUsuario[], int numerosDadosEntrada[],int quant);
+
+#endif
